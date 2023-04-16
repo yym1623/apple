3664cecad7b02da5c9682dba080610fe31d246ba
YoungMin <rhenfla@gmail.com>
2023-04-16 23:15:43 +0900

    header_insede update

---
+++ src/components/Header_inside.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+
+}
+</script>
+
+
+<template>
+  <div class="insides">
+    <!-- 1 - store -->
+    <div class="inside store"></div>
+    <!-- 2 - mac -->
+    <div class="inside mac"></div>
+    <!-- 3 - ipad -->
+    <div class="inside ipad"></div>
+    <!-- 4 - iphone -->
+    <div class="inside iphone"></div>
+    <!-- 5 - watch -->
+    <div class="inside watch"></div>
+    <!-- 6 - airpode -->
+    <div class="inside airpoad"></div>
+    <!-- 7 - tw -->
+    <div class="inside tw"></div>
+    <!-- 8 - enter -->
+    <div class="inside enter"></div>
+    <!-- 9 - acc -->
+    <div class="inside acc"></div>
+    <!-- 10 - service -->
+    <div class="inside service"></div>
+    <!-- 11 - search -->
+    <div class="inside serach"></div>
+    <!-- 12 - cart -->
+    <div class="inside cart"></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .insides {
+    .inside {
+
+    }
+  }
+</style>
